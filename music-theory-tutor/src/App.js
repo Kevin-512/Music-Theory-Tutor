3edef3819f65113ff3be4101865a535382dfb181
Kevin-512 <108360149+Kevin-512@users.noreply.github.com>
2023-10-19 14:16:05 +0100

    Navigation between pages added

--- music-theory-tutor/src/App.js
+++ music-theory-tutor/src/App.js
@@ -1,18 +1,17 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
-// import Home from "./pages/Home"
-// import About from "./pages/About";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <Navbar/>
-    // <Router>
-    //   <Navbar />
-    //   <Routes>
-    //     <Route exact path="/" component=<Home/> />
-    //     <Route path="/About" component={About} />
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
--- music-theory-tutor/src/component/Navbar.js
+++ music-theory-tutor/src/component/Navbar.js
@@ -6,24 +6,20 @@ import {
   Toolbar,
   Typography,
   Container,
+  Tabs,
+  Tab
 } from "@mui/material";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
-import About from "./pages/About";
-import Home from "./pages/Home";
+import Home from "../pages/Home";
+import About from "../pages/About";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const pages = [
-    { label: "Home", path: "/", component: Home },
-    { label: "About", path: "/about", component: About }
-    
-  ];
-
+  
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar>
-
           {/* Code for the Title and Logo */}
           <MusicNoteIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
           <Typography
@@ -44,19 +40,31 @@ const Navbar = () => {
             MUSIC THEORY TUTOR
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" },  justifyContent: "flex-end" }}>
-            {pages.map((page) => (
-              <Button
-                key={page.label}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "flex-end",
+            }}
+          >
+            <Tabs
+              aria-label="Navigation Tabs"
+              indicatorColor="secondary"
+              textColor="inherit"
+            >
+              <Tab 
+                label={"Home"}
                 component={Link}
-                to={page.path}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
+                to="/"
+                value="/" />
+              <Tab
+                label={"About"}
+                component={Link}
+                to="/about"
+                value="/about"
+              />
+            </Tabs>
           </Box>
-          
         </Toolbar>
       </Container>
     </AppBar>
@@ -64,4 +72,6 @@ const Navbar = () => {
 };
 export default Navbar;
 
-//ref https://javascript.works-hub.com/learn/how-to-create-a-responsive-navbar-using-material-ui-and-react-router-f9a01
+//ref 
+// https://javascript.works-hub.com/learn/how-to-create-a-responsive-navbar-using-material-ui-and-react-router-f9a01
+// https://github.com/marmelab/react-admin/blob/master/examples/crm/src/Header.tsx
